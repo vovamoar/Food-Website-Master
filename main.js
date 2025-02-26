4628cd66cc4a637d245c79fa315d4c19964e41a0
babihuk888 <babihuk888@gmail.com>
2025-02-26 22:10:18 +0200

    inprove button to top and added hover for cards

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 const header = document.querySelector('header')
+const scrollTopBtn = document.querySelector('.scroll')
+let menu = document.querySelector('#menu-icon')
+let navlist = document.querySelector('.navlist')
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 300) {
+		scrollTopBtn.classList.add('show')
+	} else {
+		scrollTopBtn.classList.remove('show')
+	}
+})
+
+window.addEventListener('click', () => {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth',
+	})
+})
 
 window.addEventListener('scroll', function () {
 	header.classList.toggle('sticky', window.scrollY > 80)
 })
-
-let menu = document.querySelector('#menu-icon')
-let navlist = document.querySelector('.navlist')
 
 menu.onclick = () => {
 	menu.classList.toggle('bx-x')
